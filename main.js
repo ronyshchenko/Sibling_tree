b82b604b447f97bac504ed9f57b66a71b1e9b5c9
romanoptimizator <romanoptimizator@gmail.com>
2019-05-19 18:51:46 +0300

    first

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+let treeUl = document.getElementsByTagName('ul')[0];
+let treeLis = treeUl.getElementsByTagName('li');
+
+for (let i = 0; i < treeLis.length; i++) {
+  let li = treeLis[i];
+  let span = document.createElement('span');
+  
+  li.insertBefore(span, li.firstChild);
+  span.appendChild(span.nextSibling);
+}
+
+let tree = document.getElementsByTagName('ul')[0];
+
+tree.onclick = function(event) {
+  let target = event.target;
+
+  if (target.tagName != 'SPAN') {
+    return; // клик был не на заголовке
+  }
+
+  let li = target.parentNode; // получить родительский LI
+  let childrenContainer = li.getElementsByTagName('ul')[0];
+
+  if (!childrenContainer) return; // потомков нет -- ничего не надо делать
+  childrenContainer.hidden = !childrenContainer.hidden;
+}
