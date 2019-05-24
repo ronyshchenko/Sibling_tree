891995962020e98c23bcae15df2f23751f78c415
ronyshchenko <roma.onyshchenko@gmail.com>
2019-05-24 18:15:06 +0300

    after_TA_comment

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
 'use strict';
 
-let treeUl = document.getElementsByTagName('ul')[0];
-let treeLis = treeUl.getElementsByTagName('li');
+const tree = document.querySelectorAll('ul')[0];
 
-for (let i = 0; i < treeLis.length; i++) {
-  let li = treeLis[i];
-  let span = document.createElement('span');
-  
-  li.insertBefore(span, li.firstChild);
-  span.appendChild(span.nextSibling);
-}
-
-let tree = document.getElementsByTagName('ul')[0];
-
-tree.onclick = function(event) {
+tree.addEventListener('click', (event) => {
   let target = event.target;
 
   if (target.tagName != 'SPAN') {
     return;
   }
 
-  let li = target.parentNode;
-  let childrenContainer = li.getElementsByTagName('ul')[0];
+  const li = target.parentNode;
+  const childrenContainer = li.querySelectorAll('ul')[0];
 
   if (!childrenContainer) return;
     childrenContainer.hidden = !childrenContainer.hidden;
-}
+});
